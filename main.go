c3aeb0610fb2492caa4f0b86688743be57fdfed7
Victor de Moura <76460793+VictorDMoura@users.noreply.github.com>
2025-12-18 18:22:48 +0000

    feat: add exchange rates loading functionality and rates data

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
 )
 
+type ExchangeRates struct {
+	Base  string             `json:"base"`
+	Date  string             `json:"date"`
+	Rates map[string]float64 `json:"rates"`
+}
+
+
 
 func main(){
+
+	data, err := loadRates("./rates.json")
+    if err != nil {
+        fmt.Println("Erro fatal:", err)
+        return
+    }
+	
 	arguments := os.Args[1:]
 	if len(arguments) != 2 {
 		fmt.Println("The program must be run with exactly two arguments.")
@@ -20,4 +35,23 @@ func main(){
 	}
 	coinType := arguments[1]
 	fmt.Printf("Value: %.2f, Coin Type: %s\n", value, coinType)
+	fmt.Println("Taxa do USD carregada:", data.Rates["USD"])
 }
+
+func loadRates(filepath string) (ExchangeRates, error) {
+	var rates ExchangeRates
+
+	content, err := os.ReadFile(filepath)
+	if err != nil {
+		return rates, fmt.Errorf("error reading file: %v", err)
+	}
+
+	err = json.Unmarshal(content, &rates)
+	if err != nil {
+		return rates, fmt.Errorf("error unmarshaling JSON: %v", err)
+	}
+
+	return rates, nil
+}
+
+
